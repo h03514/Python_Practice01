f92518b13ddd62876cf5c5b8b879362b0742d600
Mark <h03514@gmail.com>
2021-07-13 23:59:44 +0800

    Django練習

--- ApiTemplate/config.py
+++ ApiTemplate/config.py
@@ -2,8 +2,8 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'dd'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Qd+1'
+app.config['MYSQL_DATABASE_USER'] = 'asset'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'QveT@ipei+1'
 app.config['MYSQL_DATABASE_DB'] = 'asset'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
